8bb80d62f07713bc0f292385a031edebdcc4180a
e2402230 <e2402230@edu.vamk.fi>
2026-01-28 17:25:31 +0200

    E4

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 document.getElementById('colorBtn').addEventListener('click', () => {
     document.body.style.backgroundColor = 'lightblue';
 });
+
+
+document.getElementById("addBtn").addEventListener("click", function() {
+    
+   
+    var syote = document.getElementById("taskInput").value;
+
+    
+    var uusiRivi = document.createElement("li");
+    
+   
+    uusiRivi.textContent = syote;
+
+   
+    var poistoNappi = document.createElement("button");
+    poistoNappi.textContent = "X";
+   
+
+   
+    poistoNappi.onclick = function() {
+        uusiRivi.remove();
+    };
+
+    
+    uusiRivi.appendChild(poistoNappi);
+    document.getElementById("todolist").appendChild(uusiRivi);
+});
