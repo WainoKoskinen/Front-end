786fefb6cca4a44dcaa31a8611f89b1a8805e976
Wäinö <waino.koskinen@hotmail.com>
2026-02-03 10:30:39 +0200

    E7

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
-document.getElementById('colorBtn').addEventListener('click', () => {
-    document.body.style.backgroundColor = 'lightblue';
-});
+
+function paivitaVarasto() {
+    const tehtavat = [];
+  
+    document.querySelectorAll("#todolist li").forEach(li => {
+        
+        tehtavat.push(li.firstChild.textContent);
+    });
+    tallennaTodo(tehtavat);
+}
 
 
 document.getElementById("addBtn").addEventListener("click", function() {
-    
-   
     var syote = document.getElementById("taskInput").value;
+    if (syote === "") return; 
 
-    
+    lisaaTehtavaElementti(syote);
+    paivitaVarasto(); 
+    document.getElementById("taskInput").value = ""; 
+});
+
+function lisaaTehtavaElementti(teksti) {
     var uusiRivi = document.createElement("li");
-    
-   
-    uusiRivi.textContent = syote;
+    uusiRivi.textContent = teksti;
 
-   
     var poistoNappi = document.createElement("button");
     poistoNappi.textContent = "X";
-   
-
-   
     poistoNappi.onclick = function() {
         uusiRivi.remove();
+        paivitaVarasto(); 
     };
 
-    
     uusiRivi.appendChild(poistoNappi);
     document.getElementById("todolist").appendChild(uusiRivi);
-});
+}
+
+window.onload = function() {
+    const tallennetut = haeTodo();
+    tallennetut.forEach(tehtava => {
+        lisaaTehtavaElementti(tehtava);
+    });
+};
+
+
+function tallennaTodo(todos) {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function haeTodo() {
+    const data = localStorage.getItem("todos");
+    return data ? JSON.parse(data) : [];
+}
